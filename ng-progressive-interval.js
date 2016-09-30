89ae03523ad651dadc18374ce767b1e07749c840
retoheusser <retoheusser@windowslive.com>
2016-09-30 16:35:38 +0200

    initial commit

---
+++ ng-progressive-interval.js
@@ -0,0 +1,32 @@
+(function() {
+  "use strict";
+
+  angular
+    .module("retoheusser.progressive-interval", [])
+    .factory("progressiveInterval", ["$timeout", progressiveIntervalService]);
+
+  function progressiveIntervalService($timeout) {
+
+    return function (intervalFunction, initialDelay, maxDelay, maxCount) {
+      var executionCount = 0, running = true;
+
+      function execute() {
+        $timeout(function () {
+          if (running && (!maxCount || executionCount <= maxCount)) {
+            intervalFunction();
+            executionCount++;
+            initialDelay *= 2;
+            execute();
+          }
+        }, (initialDelay <= maxDelay) ? initialDelay : maxDelay);
+      }
+
+      execute();
+
+      return function () {
+        running = false;
+      };
+    };
+  }
+
+})();
